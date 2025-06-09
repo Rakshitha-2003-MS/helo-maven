8c67da89ed0491e31c2a61684db1c2ad17dc92ef
Rakshitha-2003-MS <rakshithams106@gmail.com>
2025-06-09 09:41:52 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package raksh.helo_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven!" );
+    }
+}
